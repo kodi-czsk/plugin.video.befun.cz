1cff8eebe816b955ec2240c496423d8f84a6a967
Libor Zoubek <lzoubek@redhat.com>
2015-04-27 23:27:35 +0200

    change install path to kodi

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 # do NOT use this script from XBMC addons directory, it is intented for development only
-DESTDIR=~/.xbmc/addons/plugin.video.befun.cz
+DESTDIR=~/.kodi/addons/plugin.video.befun.cz
 
 rm -rf ${DESTDIR}
 mkdir -p ${DESTDIR}
